f8595f9a65187247bfa453bbdef5db8438a8d22e
Alexander Sergevich <alexanderbond1966@gmail.com>
2022-01-13 09:35:45 +0200

    Update BTCCheckGen_installer.bash
    add timeout

--- BTCCheckGen_installer.bash
+++ BTCCheckGen_installer.bash
@@ -1,6 +1,7 @@
 #!/bin/bash
 echo "Hi! I will install this script in semi-automatic mode."
 echo "Привет! Я установлю этот скрипт в полуавтоматическом режиме".
+sleep 1
 apt update -y && apt upgrade -y
 apt install git python -y
 pip install requests
